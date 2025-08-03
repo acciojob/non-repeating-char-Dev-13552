d1b3cf7031d2b3ed8f5446f3cb6cb7764c3b5004
Dev-13552 <devrathour110@gmail.com>
2025-08-03 19:40:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	for(let i = 0; i < str.length; i++){
+		let count = 0;
+		for(let j = 0; j < str.length; j++){
+			if( i != j && str[i] == str[j] ){
+				count++;
+				break;
+			
+			}
+
+		}			
+		if(count == 0){
+			return str[i]
+			
+		}
+
+	}
+	return null
+	
 }
-const input = prompt("Enter a string");
+// const input = prompt("Enter a string");
+
+
 alert(firstNonRepeatedChar(input)); 
